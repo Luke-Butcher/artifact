8d30c8556b9288a274c6ceeee313da455efc34b7
Luke Butcher <butcher638@gmail.com>
2020-04-27 10:45:18 +0100

    Add files via upload
    minor tweeks and fixed the edit functionality

--- app.js
+++ app.js
@@ -1,12 +1,190 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
-const server = express();
+const bodyParser = require('body-parser');
+const app = express();
 const port = 8080;
-
-server.get("/", (req, res) => {
-    res.sendFile(__dirname + '/public/pages/index.html');
-});
-
-server.listen(port, () => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port, () => {
     console.log(`Server listening at ${port}`);
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/public/pages/index.html');
+});
+app.use(express.static('public'))
+
+app.post("/saveMinutes", (req, res) => {
+    let db = openDB();
+    let minuteId;
+    let actionId;
+    db.serialize(() => {
+        db.run(`INSERT INTO minutes(dateTime,location,attendees,userAccess,createdOn,lastUpdated,committee) VALUES(?,?,?,?,?,?,?)`, [req.body.dateTime, req.body.location, req.body.attendees, req.body.attendees, req.body.timestamp, req.body.timestamp,req.body.committee], function (err) {
+            if (err) {
+                console.log("error inserting in to minutes" + err.message);
+                res.send({error: err.message})
+            }
+            minuteId = this.lastID;
+            console.log(`A row has been inserted into minutes with rowid ${this.lastID}`);
+                db.run(`INSERT INTO actions(assignees, details,dueDate,createdOn,lastUpdated) VALUES(?,?,?,?,?)`, [req.body.assignees, req.body.details, req.body.dueDate, req.body.timestamp, req.body.timestamp], function (err) {
+                    if (err) {
+                        console.log("error inserting in to actions" + err.message);
+                        res.send({error: err.message})
+                    }
+                    actionId = this.lastID
+                    console.log(`A row has been inserted into actions with rowid ${this.lastID}`);
+                        db.run(`INSERT INTO agendaItems(relatedMinute,itemDetails,relatedActionPoint,notes,followupDate,createdOn, lastUpdated) VALUES(?,?,?,?,?,?,?)`, [minuteId, req.body.itemDetails, actionId, req.body.notes, req.body.followupDate, req.body.timestamp, req.body.timestamp], function (err) {
+                            if (err) {
+                                console.log("error inserting in to agendaItems" + err.message);
+                                res.send({error: err.message})
+                            }
+                            console.log(`A row has been inserted into agendaItems with rowid ${this.lastID}`);
+                            closeDB(db);
+
+                        });
+
+
+                });
+
+        });
+    });
+    res.send({success:"success"})
+});
+
+app.post("/updateMinutes", (req, res) => {
+    let db = openDB();
+    let minuteId;
+    let actionId;
+    db.serialize(() => {
+        db.run(`UPDATE minutes set dateTime = ?,  location = ?,  attendees = ?,  userAccess = ?,  createdOn = ?, lastUpdated = ?,  committee = ? where minuteID = ?`
+, [req.body.dateTime, req.body.location, req.body.attendees, req.body.attendees, req.body.timestamp, req.body.timestamp,req.body.committee, req.body.minuteID], function (err) {
+            if (err) {
+                console.log("error updating minutes" + err.message);
+                res.send({error: err.message})
+            }
+            console.log(`A row has been updated in minutes with rowid ${this.lastID}`);
+            db.run(`update actions
+            set assignees = ?,
+              details = ?,
+              dueDate = ?,
+              createdOn = ?,
+              lastUpdated = ?
+             where actionID = ?`, [req.body.assignees, req.body.details, req.body.dueDate, req.body.timestamp, req.body.timestamp,req.body.actionID], function (err) {
+                if (err) {
+                    console.log("error updating in to actions" + err.message);
+                    res.send({error: err.message})
+                }
+                console.log(`A row has been updated in actions with rowid ${this.lastID}`);
+                db.run(`update agendaItems
+                set relatedMinute = ?,
+                 itemDetails =?,
+                 relatedActionPoint = ?,
+                 notes = ?,
+                 followupDate = ?,
+                 createdOn = ?,
+                  lastUpdated = ?
+                 where agendaItemID = ?`, [req.body.minuteID, req.body.itemDetails, req.body.actionID, req.body.notes, req.body.followupDate, req.body.timestamp, req.body.timestamp, req.body.agendaItemID], function (err) {
+                    if (err) {
+                        console.log("error updating agendaItems" + err.message);
+                        res.send({error: err.message})
+                    }
+                    console.log(`A row has been updated in agendaItems with rowid ${this.lastID}`);
+                    closeDB(db);
+
+                });
+            });
+        });
+    });
+    res.send({success:"success"})
+});
+
+app.post("/loadSelectableMinutes", (req,res)=> {
+    let username = req.body.username + '%';
+    console.log(username)
+    let db = openDB()
+    let sql = `SELECT * FROM minutes m
+     left outer join agendaItems aI on m.minuteID = aI.relatedMinute
+     left outer join actions a on aI.relatedActionPoint = a.actionID
+where userAccess like (?)
+ORDER BY -m.createdOn`;
+
+    db.all(sql, [username], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result)
+        res.send({success:"success",result:result})
+    });
+    closeDB(db);
+});
+
+app.post("/login", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let db = openDB();
+    let sql = `SELECT * FROM users
+           WHERE username LIKE (?) AND password = (?)`;
+
+    db.all(sql, [username, password], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        if (result.length === 1){
+            res.send({userExists:true,username:username})
+        } else {
+            res.send({userExists:false})
+        }
+    });
+    closeDB(db);});
+
+app.post("/signup", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let db = openDB();
+    let sql = `SELECT * FROM users
+           WHERE username LIKE (?) AND password = (?)`;
+
+    db.all(sql, [username, password], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        if (result.length === 1){
+            res.send({userExists:true})
+        } else {
+            db.run(`INSERT INTO users(username,password,createdOn,lastUpdated) VALUES(?,?,?,?)`, [req.body.username,req.body.password,req.body.createdOn,req.body.createdOn], function(err) {
+                if (err) {
+                    console.log(err.message);
+                    res.send({error:err.message})
+                } else {
+                    res.send({userExists:false});
+                    console.log(`A row has been inserted with rowid ${this.lastID}`);
+                }
+            });
+        }
+    });
+    closeDB(db);});
+
+function openDB(){
+    let db = new sqlite3.Database('./db/Database0.db', sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        else {
+            db.get("PRAGMA foreign_keys = ON")
+            console.log('Connected to Database0');}
+    });
+    return db
+}
+
+function closeDB(db) {
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('connection closed');
+    });
+}
---
+++ public/scripts/index.js
@@ -0,0 +1,163 @@
+const loginModal = document.getElementById('loginModal');
+const signupModal = document.getElementById('signupModal');
+window.onload = function() {
+    if (sessionStorage.getItem("username") == null && localStorage.getItem("username") == null){
+        showModal(loginModal)
+    }
+};
+window.onclick = function(event) {
+    if (event.target === loadModal) {
+        closeLoadModal()
+    }};
+const loadModal = document.getElementById("loadModal");
+const saveButton = document.getElementById('saveButton');
+const loadButton = document.getElementById('loadButton');
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+const minuteForm = document.getElementById('minuteForm');
+
+saveButton.addEventListener('click', async () => {
+    let minuteFormData = new FormData(minuteForm);
+    minuteFormData.append('timestamp', new Date().toJSON());
+    let data = JSON.stringify(Object.fromEntries(minuteFormData));
+    //if the minuteID is populated this is a loaded content set and save should do an update
+    if(document.getElementById("minuteID").value){
+        const response = await fetch('updateMinutes', {
+            method: 'POST',
+            body: data,
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(() => alert("success!!"));
+    }else{
+        const response = await fetch('saveMinutes', {
+            method: 'POST',
+            body: data,
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(() => alert("success!!"));
+    }
+});
+loadButton.addEventListener('click', () => {
+    showModal(loadModal);
+    buildLoadModalOptions()
+});
+loginForm.addEventListener('submit', async () => {
+    event.preventDefault();
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let remember = document.getElementById('remember').checked;
+    let data = {username:username,password:password}
+    const response = await fetch('login', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json() )
+        .catch(error => console.error('Error:', error))
+        .then(response => {
+            if (response.userExists === true){
+                if (remember === true){
+                    window.localStorage.setItem('username', response.username);
+                }else {
+                    window.sessionStorage.setItem('username', response.username);
+                }
+                alert("success!!");
+                closeModal(loginModal)
+            } else if (response.userExists === false ){
+                alert("incorrect username or password")
+            }
+        })
+});
+signupForm.addEventListener('submit', async () => {
+    event.preventDefault();
+    let username = document.getElementById('signupUsername').value;
+    let password = document.getElementById('signupPassword').value;
+    let createdOn = new Date().toJSON();
+    let data = {username:username,password:password,createdOn:createdOn}
+    const response = await fetch('signup', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json() )
+        .catch(error => console.error('Error:', error))
+        .then(response => {
+            if (response.userExists === true){
+                alert("Your username is not unique! please try again")
+
+            } else if (response.userExists === false  ){
+                window.sessionStorage.setItem('username', username);
+                //this needs better userfeed back so they know why it closed
+                alert("success!!");
+                closeModal(signupModal)
+            } else if (response.error.length > 0){
+                alert("sorry something went wrong")
+            }
+        })
+});
+
+
+async function buildLoadModalOptions() {
+    let username;
+    if(sessionStorage.getItem("username")!= null){
+        username = sessionStorage.getItem("username")
+    } else {
+        username=localStorage.getItem("username")
+    }
+    const response = await fetch('loadSelectableMinutes', {
+        method: 'POST',
+        body: JSON.stringify({username:username}),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json() )
+        .catch(error => console.error('Error:', error))
+        .then(response =>{
+            const minuteList = document.getElementById("minuteList");
+            for (let i in response.result){
+                let date = response.result[i].createdOn;
+                let data = response.result[i].committee;
+                let el = makeClickableListItem(date, data, response.result[i]);
+                minuteList.appendChild(el);
+            }
+        })};
+function makeClickableListItem(date, data, json) {
+    const node = document.createElement("LI");
+    let textnode = document.createTextNode(date + "    " + data);
+    node.appendChild(textnode);
+    node.addEventListener('click', () => {
+        let inputs = Array.prototype.slice.call(document.querySelectorAll('form input'));
+//inputItem.name must match the column in the db for this to work
+        Object.keys(json).map(function (dataItem) {
+            inputs.map(function (inputItem) {
+                return (inputItem.name === dataItem) ? (inputItem.value = json[dataItem]) : false;
+            });
+        });
+
+        closeLoadModal()});
+    return node;
+}
+function showModal(modal) {
+    modal.style.display = 'block';
+    document.getElementById('page-mask').style.display = 'block';
+}
+function closeModal(modal){
+    modal.style.display = "none";
+    document.getElementById('page-mask').style.display='none';
+
+}
+function closeLoadModal(){
+    closeModal(loadModal)
+    let root = document.getElementById("minuteList");
+    while( root.firstChild ){
+        root.removeChild( root.firstChild );
+    }
+}
